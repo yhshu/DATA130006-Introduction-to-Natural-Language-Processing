af8d09a5bb8ee05709b95d2d79bbfd3b01cedce5
Yiheng Shu <shuyiheng29@gmail.com>
2019-07-25 23:18:42 +0800

    Update eval.py

--- Assignment1/eval.py
+++ Assignment1/eval.py
@@ -5,7 +5,7 @@ anspath = './ans.txt'                # 真实结果
 resultpath = './result.txt'          # 模型预测结果
 ansfile = open(anspath, 'r')
 resultfile = open(resultpath, 'r')
-count = 0
+match_count = 0
 n_sentence = 1000                    # 句子数量
 for i in range(n_sentence):
     ansline = ansfile.readline().split('\t')[1]
@@ -13,5 +13,5 @@ for i in range(n_sentence):
     resultline = resultfile.readline().split('\t')[1]
     resultset = set(nltk.word_tokenize(resultline))
     if ansset == resultset:
-        count += 1
-print("Accuracy is : %.2f%%" % (count * 1.00 / 10))
+        match_count += 1
+print("Accuracy is : %.4f%%" % (match_count * 100.00 / n_sentence))
