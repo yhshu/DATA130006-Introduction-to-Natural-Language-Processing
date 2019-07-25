064c9c7dc6ee92e795265defb50f44d9cf75b9c3
Yiheng Shu <shuyiheng29@gmail.com>
2019-07-25 22:01:23 +0800

    完成 readme

--- Assignment1/eval.py
+++ Assignment1/eval.py
@@ -1,14 +1,17 @@
+# coding=utf-8
 import nltk
-anspath='./ans.txt'
-resultpath='./result.txt'
-ansfile=open(anspath,'r')
-resultfile=open(resultpath,'r')
-count=0
-for i in range(1000):
-    ansline=ansfile.readline().split('\t')[1]
-    ansset=set(nltk.word_tokenize(ansline))
-    resultline=resultfile.readline().split('\t')[1]
-    resultset=set(nltk.word_tokenize(resultline))
-    if ansset==resultset:
-        count+=1
-print("Accuracy is : %.2f%%" % (count*1.00/10))
+
+anspath = './ans.txt'                # 真实结果
+resultpath = './result.txt'          # 模型预测结果
+ansfile = open(anspath, 'r')
+resultfile = open(resultpath, 'r')
+count = 0
+n_sentence = 1000                    # 句子数量
+for i in range(n_sentence):
+    ansline = ansfile.readline().split('\t')[1]
+    ansset = set(nltk.word_tokenize(ansline))
+    resultline = resultfile.readline().split('\t')[1]
+    resultset = set(nltk.word_tokenize(resultline))
+    if ansset == resultset:
+        count += 1
+print("Accuracy is : %.2f%%" % (count * 1.00 / 10))
